024128ae7af3ccf5ebffe54fad3b4e9b4c59ce06
Madi <madi_gaziz@mail.ru>
2023-06-21 16:00:52 +0600

    Homework_44 added fields for displaying data in the admin panel

--- To_do_list/admin.py
+++ To_do_list/admin.py
@@ -3,8 +3,8 @@ from To_do_list.models import Task
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'description', 'status', 'date_of_completion']
-    list_filter = ['date_of_completion']
+    list_display = ['id', 'name', 'description', 'status', 'start_date', 'date_of_completion']
+    list_filter = ['start_date', 'date_of_completion']
     search_fields = ['description']
 
 
