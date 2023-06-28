6141b53fe5c55b568300d32b9ccc15126418d2d3
Madi <madi_gaziz@mail.ru>
2023-06-29 03:21:31 +0600

    Добавил в админ панель status

--- To_do_list/admin.py
+++ To_do_list/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from To_do_list.models import Task
+from To_do_list.models import Task, Status
 
 
 class TaskAdmin(admin.ModelAdmin):
@@ -7,6 +7,12 @@ class TaskAdmin(admin.ModelAdmin):
     list_filter = ['start_date', 'date_of_completion']
     search_fields = ['description']
 
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['name']
+    search_fields = ['name']
+
 
 admin.site.register(Task, TaskAdmin)
+admin.site.register(Status, StatusAdmin)
 # Register your models here.
