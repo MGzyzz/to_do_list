17727a8029f882905e15f5a3ae49621bc1d5159a
Madi <madi_gaziz@mail.ru>
2023-06-29 03:26:25 +0600

    Добавил новую функцию task_edit, а так же добавил изменение в нынешние функции такие как views_all, new_task, task_detail_view

--- To_do_list/views.py
+++ To_do_list/views.py
@@ -1,33 +1,66 @@
-from django.shortcuts import render, redirect
-from .models import Task
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task, Status
+from To_do_list.forms import TaskForms
 # Create your views here.
 
 
 def views_all(request):
-    return render(
-        request, 'home.html', context={
-            'tasks': Task.objects.order_by('description')
-        }
+    if request.method == 'POST':
+        selected_tasks = request.POST.getlist('task')
+        for i in selected_tasks:
+            Task.objects.filter(id=i).delete()
+        return redirect('home')
+    else:
+        return render(
+            request, 'home.html', context={
+                'tasks': Task.objects.order_by('description')
+            }
     )
 
 
 def new_task(request):
     if request.method == 'POST':
-        data = request.POST
-        task_data = {
-            'name': data.get('name'),
-            'description': data.get("description"),
-            'status': data.get("status"),
-            'start_date': data.get('start_date'),
-            'date_of_completion': data.get("completion_date")
-        }
-        task = Task.objects.create(**task_data)
-        return redirect('task-detail', id=task.id)
+        form = TaskForms(request.POST)
+        if form.is_valid():
+            task = Task.objects.create(
+                name=request.POST.get('name'),
+                description=request.POST.get('description'),
+                status_id=request.POST.get('status'),
+                start_date=request.POST.get('start_date'),
+                date_of_completion=request.POST.get('date_of_completion')
+            )
+            return redirect('task-detail', id=task.id)
     else:
-        context = {'status_choices': Task.TASK_STATUS_CHOICE}
-        return render(request, 'task_create.html', context)
+        form = TaskForms()
+    return render(request, 'task_create.html', {'form': form, 'status': Status.objects.all()})
 
 
 def task_detail_view(request, id: int):
-    task = Task.objects.get(id=id)
+    task = get_object_or_404(Task, id=id)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('home')
+
     return render(request, 'task_detail.html', context={'task': task})
+
+
+def task_edit(request, id):
+    task = get_object_or_404(Task, id=id)
+    if request.method == 'GET':
+        form = TaskForms(initial={
+            'name': task.name,
+            'description': task.description,
+            'status': task.status
+        })
+        return render(request, 'task_edit.html', {'task': task, 'status': Status.objects.all(), 'form': form})
+    elif request.method == 'POST':
+        form = TaskForms(data=request.POST)
+        print(form)
+        if form.is_valid():
+            task.name = request.POST.get('name')
+            task.description = request.POST.get('description')
+            task.status_id = request.POST.get('status')
+            task.save()
+            return redirect('task-detail', id=task.id)
+        else:
+            return render(request, 'task_edit.html', {'task': task, 'status': Status.objects.all(), 'form': form})
